29240391d1245a62dcce3af5cde24334160bcbfa
ashu01 <as01hu@gmail.com>
2016-06-16 18:03:27 +0530

    read all .mp4 files

--- readvideofiles.go
+++ readvideofiles.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
+	"os"
+	// "bufio"
+	"log"
+	// "io"
+	// "time"
 )
 
+// Reading files requires checking most calls for errors.
+// This helper will streamline our error checks below.
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	files, _ := filepath.Glob("*")
-	fmt.Println(files)
+
+	file, err := os.Open("./Video/cat.mp4")
+	check(err)
+
+	data := make([]byte, 100)
+	count, err1 := file.Read(data)
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	// var z int64
+	// z = int64(count)
+	// o2, err := file.Seek(z, 1)
+	fmt.Println(count)
+	fmt.Printf("\n\n\n\n\n\n")
+	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+
+	// tobestreamed := make([]byte, 100)
 }
